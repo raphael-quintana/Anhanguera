3a720d5c6f203aae078b05c694be042a3dbcbe83
raphael-quintana <rapha.quintana13@gmail.com>
2026-03-15 19:50:40 -0300

    Updating

--- anhanguera/entrada_montanha_russa.c
+++ anhanguera/entrada_montanha_russa.c
@@ -7,17 +7,20 @@ int main(){
           
         printf("\nInforme sua altura, por favor: \n");
         scanf("%f", &altura);  
-   
 
-        if (altura >= 1.70){
+        if (altura >= 1.70) {
             printf("Você tem acesso VIP!\n");
         }
-        else if (altura >= 1.50){
+
+        else if (altura >= 1.50) {
             printf("Bem vindo!\n");
         }
-        else{
+
+        else {
             printf("Acesso Negado\n");
         }
+
     }
-return 0;
+
+    return 0;
 }
