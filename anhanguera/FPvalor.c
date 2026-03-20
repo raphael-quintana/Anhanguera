ecaa45b7d25d0052e65ad52f8be958485006874c
raphael-quintana <rapha.quintana13@gmail.com>
2026-03-19 22:04:43 -0300

    Aula de Funções (Passagem de Valor)

---
+++ anhanguera/FPvalor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int fatorial(int n) {
+    int res = 1;
+    
+    if (n == 0)
+        return 1;
+    else {
+        for (int i = n; i > 1; i--) {
+            res = res * i;
+        }
+    }    
+    
+    
+    return res;
+
+}
+
+int main() {
+    int numero, res;
+
+    printf("Digite um numero inteiro positivo: ");
+    scanf("%d", &numero);
+
+    if (numero < 0)
+        printf("Numero negativo!");
+    else
+        printf("resultado = %d", fatorial(numero));
+
+    return 0;
+}
