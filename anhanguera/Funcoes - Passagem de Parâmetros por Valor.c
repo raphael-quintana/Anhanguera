a42f9bb9d1f32cd91b7d8e9cf106d5a3ee24a6ff
raphael-quintana <rapha.quintana13@gmail.com>
2026-03-21 10:20:11 -0300

    Encerramento - Funcoes

--- anhanguera/Funcoes - Passagem de Parâmetros por Valor.c
+++ anhanguera/Funcoes - Passagem de Parâmetros por Valor.c
@@ -11,9 +11,7 @@ int fatorial(int n) {
         }
     }    
     
-    
     return res;
-
 }
 
 int main() {
---
+++ anhanguera/Testes.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+
+int main() {
+
+    int num;
+
+    printf("Digite um numero: ");
+
+    scanf("%d", &num);
+
+    printf("O numero digitado foi: %d", num);
+
+    return 0;
+
+}
