4b7f7b23745a062c8888ac133806c5051751fe0c
raphael-quintana <rapha.quintana13@gmail.com>
2026-03-26 19:36:43 -0300

    Finalizing  U3A1 Exercise.

--- anhanguera/AulaPraticaU3A1.c
+++ anhanguera/AulaPraticaU3A1.c
@@ -1,2 +1,20 @@
-# include <stdio.h>
+#include <stdio.h>
 
+int main () {
+    int vendas[5], somaVendaSemana = 0;
+    
+    for (int i = 0; i < 5; i++) {
+        printf("Digite seu total de vendas de hoje: \n");
+        scanf("%d", &vendas[i]);
+        somaVendaSemana += vendas[i]; 
+    }
+
+    printf("\n\nRelatório Semanal de Vendas\n\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Dia %d: %d reais.\n", i + 1, vendas[i]);
+    }
+    
+    printf("\nO Total de vendas da semana foi de %d reais!\n\n", somaVendaSemana);
+
+    return 0;
+}
