bbbfb8a699b938e3b4d9205b7dd041d8deeaefe0
raphael-quintana <rapha.quintana13@gmail.com>
2026-07-13 20:10:58 -0300

    Updating

--- Atividades_Praticas/Atividade1.py
+++ Atividades_Praticas/Atividade1.py
@@ -1,12 +1,11 @@
 n = int(input("Type your number:"))
 
 if n % 2 == 0:
-    if  n >= 2 and n <= 5:
+    if n >= 2 and n <= 5:
         print("Not weird")
     elif n >= 6 and n <= 20:
         print("Weird")
     elif n > 20:
         print("Not Weird")
-else:
-    print("Weird")
-
+    else:
+        print("Weird")
